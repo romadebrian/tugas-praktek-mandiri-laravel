ed666bd4239f91f8940cc3139720bc0d162575a5
romadebrian <roma_coll04@yahoo.com>
2024-11-22 03:24:54 +0700

    styling image

--- resources/views/viewNews.blade.php
+++ resources/views/viewNews.blade.php
@@ -1,15 +1,22 @@
 @extends('index')
 
 @section('main')
-    <div class="blog-post">
-        <h2 class="blog-post-title">{{ $data->title }}</h2>
-        <p class="blog-post-meta">{{ $data->author }}</p>
-        <p class="blog-post-meta">{{ $data->created_at }}</p>
+    <div class="container">
+        <div class="blog-post">
+            <h2>{{ $data->title }}</h2>
+            <p>{{ $data->author }}</p>
+            <p style="margin-top: -10px">{{ $data->created_at }}</p>
 
-        {{-- {{ $data->content }} --}}
-        @php
-            echo $data->content;
-        @endphp
+            <div class="" style="display: flex; justify-content: center">
+                <img src="{{ asset('storage/' . $data->image) }}" alt="{{ $data->image }}"
+                    style="width: 300px; height: 200px; border-radius: 10px;">
+            </div>
 
-    </div><!-- /.blog-post -->
+            {{-- {{ $data->content }} --}}
+            @php
+                echo $data->content;
+            @endphp
+
+        </div><!-- /.blog-post -->
+    </div>
 @endsection
