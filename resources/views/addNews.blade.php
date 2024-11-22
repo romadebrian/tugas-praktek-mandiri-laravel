eeccfc9fb70b6c898f54fa29c3c6d5959125cc78
romadebrian <roma_coll04@yahoo.com>
2024-11-22 14:16:27 +0700

    value checkbox to array

--- resources/views/addNews.blade.php
+++ resources/views/addNews.blade.php
@@ -51,6 +51,51 @@
 
             <textarea id="myeditorinstance" name="content">{{ old('content') }}</textarea>
         </div>
+
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" name="namecheckbox1" value="valcheckbox1">
+            <label class="form-check-label" for="inlineCheckbox1">1</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" name="namecheckbox2" value="valcheckbox2">
+            <label class="form-check-label" for="inlineCheckbox2">2</label>
+        </div>
+
+
+
+
+
+        <div class="row" style="">
+            @for ($i = 0; $i < 23; $i++)
+                <div class="checkbox-wrapper-4">
+
+                    <input class="inp-cbx" id="checkbox{{ $i }}" type="checkbox" name="checkbox[]"
+                        value="ValCheckbox{{ $i }}" />
+                    <label class="cbx" for="checkbox{{ $i }}"><span>
+                            <svg width="12px" height="10px">
+                                <use xlink:href="#check-4"></use>
+                            </svg></span><span>checkbox{{ $i }}</span></label>
+                    <svg class="inline-svg">
+                        <symbol id="check-4" viewbox="0 0 12 10">
+                            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                        </symbol>
+                    </svg>
+                </div>
+            @endfor
+
+            <div class="btn btn-primary"
+                style="width: 101px; height: 30px; font-size: 12px; font-weight: 400; padding: 6px 8px; margin-bottom: .5rem;">
+                Add
+                Category</div>
+        </div>
+
+
+
+
+
+
+
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
