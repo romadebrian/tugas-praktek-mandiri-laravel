a62c2d70298d1d47b0724714b7f94e1c596af0f2
romadebrian <roma_coll04@yahoo.com>
2024-11-29 14:44:27 +0700

    ubah ke bahasa indonesia

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,16 @@
                 <div>
                     <div class="d-flex flex-row">
                         <div class="p-2">
-                            <img src="{{ asset('storage/' . $item->image) }}" alt="Event Image"
+                            <img src="{{ asset('storage/' . $item->foto) }}" alt="Event Image"
                                 style="width: 300px; height: 200px; border-radius: 10px;">
 
                         </div>
                         <div class="p-2">
                             <h3 class="" style="font-weight: bold; margin-top: 10px;">
-                                <a href="{{ URL::to('post', $item->id) }}">{{ $item->title }}</a>
+                                <a href="{{ URL::to('post', $item->id) }}">{{ $item->judul }}</a>
                             </h3>
                             <div class="" style="margin-top: 0px;">
-                                <p>{{ $item->description }}</p>
+                                <p>{{ $item->deskripsi }}</p>
                                 <p style="margin-top: -10px"> {{ $item->created_at }} </p>
                             </div>
                         </div>
