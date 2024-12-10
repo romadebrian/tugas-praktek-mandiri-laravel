0b24e1bdba567fb7a5e52e95c2cecfc8a4738a3e
romadebrian <roma_coll04@yahoo.com>
2024-12-10 20:23:19 +0700

    add related produk berdasarkan kategori

--- resources/views/viewNews.blade.php
+++ resources/views/viewNews.blade.php
@@ -18,5 +18,43 @@
             @endphp
 
         </div><!-- /.blog-post -->
+
+        <div style="margin-top: 5rem;"></div>
+        <hr class="featurette-divider">
+        <div class="row">
+            @foreach ($dataProduk as $itemProduk)
+                @foreach ($itemProduk->kategori as $itemKategori)
+                    @foreach ($data->kategori as $kategoriPost)
+                        @if ($kategoriPost === $itemKategori)
+                            {{ $kategoriPost }}
+                            <div class="col-md-4">
+                                <div class="card mb-4 box-shadow">
+                                    <img class="card-img-top"
+                                        data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
+                                        alt="Card image cap">
+                                    <div class="card-body">
+                                        <p class="card-text">This is a wider card with supporting text below as a natural
+                                            lead-in to
+                                            additional content. This content is a little bit longer.</p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button"
+                                                    class="btn btn-sm btn-outline-secondary">View</button>
+                                                <button type="button"
+                                                    class="btn btn-sm btn-outline-secondary">Edit</button>
+                                            </div>
+                                            <small class="text-muted">{{ $itemProduk->harga }}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                @endforeach
+            @endforeach
+
+
+
+        </div>
     </div>
 @endsection
