cdd4de0fb22d1860fb7ce410513df2ebcf2be3a7
romadebrian <roma_coll04@yahoo.com>
2024-12-25 16:59:22 +0700

    margin

--- resources/views/viewNews.blade.php
+++ resources/views/viewNews.blade.php
@@ -7,7 +7,7 @@
             <p>{{ $data->penulis }}</p>
             <p style="margin-top: -10px">{{ $data->created_at }}</p>
 
-            <div class="" style="display: flex; justify-content: center">
+            <div class="mb-5" style="display: flex; justify-content: center">
                 <img src="{{ asset('storage/' . $data->foto) }}" alt="{{ $data->image }}"
                     style="width: 300px; height: 200px; border-radius: 10px;">
             </div>
